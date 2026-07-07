a434600e25e46c6cfa925e8d225122598c71ba0e
Yi Jay <wer35073@gmail.com>
2026-07-07 14:35:05 +0800

    feat:商品描述+加入購物車+收藏按鈕+立即購買按鈕

---
+++ app/shop/_components/Favorite.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Favorite() {
+  return (
+    <div className="w-full">
+      <div
+        className="flex items-center gap-1.5 px-3 py-1 h-12 justify-center
+                      bg-[#FCF9F6] text-[#3D2419] font-bold text-sm
+                      border-[3px] border-[#3D2419]
+                      shadow-[2px_2px_0px_0px_#3D2419] select-none"
+      >
+        <button>收藏</button>
+      </div>
+    </div>
+  );
+}
--- app/shop/products/[id]/page.tsx
+++ app/shop/products/[id]/page.tsx
@@ -5,6 +5,10 @@ import Hashtag from "./_component/Hashtag";
 import SpecSelector from "./_component/SpecSelector";
 import ProductAddOns from "./_component/ProductAddOns";
 import QuantityPicker from "../../_components/QuantityPicker";
+import CartButtons from "../../_components/CartButtons";
+import Favorite from "../../_components/Favorite";
+import PurchaseButton from "./_component/PurchaseButton";
+import ProductDescription from "./_component/ProductDescription";
 
 export default function page() {
   return (
@@ -18,30 +22,51 @@ export default function page() {
             <ProductPhoto />
           </div>
           {/* 右上購買區塊 */}
-          <div className="w-full bg-blue-300">
-            <div className="flex items-center justify-center w-full h-20 bg-purple-300">
+          <div className="flex flex-col items-center justify-center w-full">
+            <div className="flex items-center justify-center w-full h-20">
               <span className="text-2xl">
                 川媽臭臭鍋 - 臭到要他命套裝餐券(待確認風格)
               </span>
             </div>
-            <div className="ml-4 mb-4 mt-4">
-              <Hashtag/>
-            </div>
-            <div className="ml-4 mb-4 mt-6">
-              <span className="text-3xl">＄價格</span>
-            </div>
-            <div className="ml-4 mb-4 mt-6">
-              <SpecSelector/>
-            </div>
-            <div className="ml-4 mb-4 mt-6">
-              <ProductAddOns/>
-            </div>
-            <div className="flex items-center ml-4 mb-4 mt-6 gap-3">
-              <QuantityPicker/>
-              <span>可購買數量: 99+</span>
+            <div className="w-[80%]">
+              {/* 熱門標籤 */}
+              <div className="ml-4 mb-4 mt-4">
+                <Hashtag />
+              </div>
+              <div className="ml-4 mb-4 mt-6">
+                <span className="text-3xl">＄價格</span>
+              </div>
+              {/* 規格 */}
+              <div className="ml-4 mb-4 mt-6">
+                <SpecSelector />
+              </div>
+              {/* 加購區 */}
+              <div className="ml-4 mb-4 mt-6">
+                <ProductAddOns />
+              </div>
+              {/* 數量按鈕 */}
+              <div className="flex items-center ml-4 mb-4 mt-6 gap-3">
+                <QuantityPicker />
+                <span>可購買數量: 99+</span>
+              </div>
+              {/* 加入購物車與收藏 */}
+              <div className="flex ml-4 mb-4 mt-6 gap-3">
+                <CartButtons />
+                <div className="w-fit">
+                <Favorite />
+                </div>
+              </div>
+              {/* 立即購買按鈕 */}
+              <div className="flex ml-4 mb-4 mt-6 gap-3">
+                <PurchaseButton />
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      {/* 下方商品說明區塊 */}
+      <div>
+        <ProductDescription/>
       </div>
     </div>
   );
