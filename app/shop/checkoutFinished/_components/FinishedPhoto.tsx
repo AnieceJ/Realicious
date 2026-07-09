3ccdf5d87d4632995e92e6bce5086599e42ba557
Yi Jay <wer35073@gmail.com>
2026-07-09 12:06:28 +0800

    feat:結帳完成-訂單明細

---
+++ app/shop/checkoutFinished/_components/FinishedPhoto.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function FinishedPhoto() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="bg-purple-300 w-100 h-50 mb-4">
+        {/* <img src="" alt="" /> */}
+      </div>
+    </div>
+  );
+}
