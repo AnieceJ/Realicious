17bfa8cbe05fa7303137f34671d1580250d2fd0d
Yi Jay <wer35073@gmail.com>
2026-07-09 09:39:26 +0800

    feat:結帳-支付方式

--- app/shop/checkout/page.tsx
+++ app/shop/checkout/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import CheckoutContactInfo from "./_components/CheckoutContactInfo";
 import CheckoutOrderList from "./_components/CheckoutOrderList";
+import CheckoutPaymentMethod from "./_components/CheckoutPaymentMethod";
 
 export default function CheckoutPage() {
   return (
@@ -9,13 +10,17 @@ export default function CheckoutPage() {
       <div className="fixed inset-0 -z-10 bg-[#FFFFFF]" />
       {/* 左側+右側 */}
       <div className="max-w-7xl">
+        <h2 className="text-4xl mb-6">結帳/CHECKOUT</h2>
         {/* 左側 */}
         <div className="w-[60%]">
           <div className="mb-6">
             <CheckoutContactInfo />
           </div>
+          <div className="mb-6">
+            <CheckoutOrderList />
+          </div>
           <div>
-            <CheckoutOrderList/>
+            <CheckoutPaymentMethod />
           </div>
         </div>
       </div>
