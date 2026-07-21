06460b4d4cf2e23c93e6c12069930240c6499e16
Yi Jay <wer35073@gmail.com>
2026-07-21 11:36:04 +0800

    update: 改userSidebar的導航至訂單紀錄

--- app/_components/_components/userSidebar.tsx
+++ app/_components/_components/userSidebar.tsx
@@ -116,10 +116,10 @@ export default function UserSidebar() {
                   <Link
                     onClick={() => setIsOpening(false)}
                     className="w-full h-12.5 text-left pl-8 cursor-pointer hover:bg-[#FBDF58] flex items-center"
-                    href={`/user/account/cart`}
+                    href={`/user/account/orders`}
                   >
                     <FaShoppingCart />
-                    <span className="ml-4">購買記錄</span>
+                    <span className="ml-4">訂單記錄</span>
                   </Link>
                   <Link
                     onClick={() => setIsOpening(false)}
